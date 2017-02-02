2ec92634e078e33724c8bd809070ccdb931b8b82
Matthew <matthew.ch.robbins@gmail.com>
2017-02-03 09:57:56 +1100

    Initial WIP for lottie forms renderers

---
+++ Droid/Renderers/AnimationViewRenderer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Lottie.Forms.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(Lottie.Forms.AnimationView), typeof(AnimationViewRenderer))]
+
+namespace Lottie.Forms.Droid.Renderers
+{
+	public class AnimationViewRenderer : ViewRenderer<Lottie.Forms.AnimationView, Com.Airbnb.Lottie.LottieAnimationView>
+	{
+		public AnimationViewRenderer()
+		{
+		}
+
+		Com.Airbnb.Lottie.LottieAnimationView animationView;
+
+		protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<AnimationView> e)
+		{
+			base.OnElementChanged(e);
+
+            if (Control == null)
+			{
+				animationView = new Com.Airbnb.Lottie.LottieAnimationView(Context);
+				SetNativeControl(animationView);
+			}
+
+			if (e.OldElement != null)
+			{
+				e.OldElement.OnPlay -= this.OnPlay;
+				e.OldElement.OnPause -= this.OnPause;
+			}
+
+			if (e.NewElement != null)
+			{
+				e.NewElement.OnPlay += this.OnPlay;
+				e.NewElement.OnPause += this.OnPause;
+			}
+		}
+
+		void OnPlay(object sender, EventArgs e)
+		{
+			if (animationView != null 
+			    && animationView.Handle != IntPtr.Zero)
+			{
+				animationView.PlayAnimation();
+			}
+		}
+
+		void OnPause(object sender, EventArgs e)
+		{
+			if (animationView != null
+				&& animationView.Handle != IntPtr.Zero)
+			{
+				animationView.PauseAnimation();
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == AnimationView.AnimationProperty.PropertyName)
+			{
+				animationView.SetAnimation(Element.Animation);
+			}
+
+			if (e.PropertyName == AnimationView.ProgressProperty.PropertyName)
+			{
+				animationView.Progress = Element.Progress;
+			}
+			
+			base.OnElementPropertyChanged(sender, e);
+		}
+	}
+}
---
+++ Lottie.Forms/AnimationView.cs
@@ -0,0 +1,47 @@
+using System;
+namespace Lottie.Forms
+{
+	public class AnimationView : Xamarin.Forms.ContentView
+	{
+		public static readonly Xamarin.Forms.BindableProperty ProgressProperty = Xamarin.Forms.BindableProperty.Create(nameof(Progress), typeof(float), typeof(Lottie.Forms.AnimationView), default(float));
+		public float Progress
+		{
+			get
+			{
+				return (float)GetValue(ProgressProperty);
+			}
+
+			set
+			{
+				SetValue(ProgressProperty, value);
+			}
+		}
+
+		public static readonly Xamarin.Forms.BindableProperty AnimationProperty = Xamarin.Forms.BindableProperty.Create(nameof(Animation), typeof(string), typeof(Lottie.Forms.AnimationView), default(string), Xamarin.Forms.BindingMode.OneWay);
+		public string Animation
+		{
+			get
+			{
+				return (string)GetValue(AnimationProperty);
+			}
+
+			set
+			{
+				SetValue(AnimationProperty, value);
+			}
+		}
+
+		public event EventHandler OnPlay;
+
+		public void Play()
+		{
+			OnPlay?.Invoke(this, new EventArgs());
+		}
+
+		public event EventHandler OnPause;
+		public void Pause()
+		{
+			OnPause?.Invoke(this, new EventArgs());
+		}
+	}
+}
