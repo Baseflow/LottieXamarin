78bd165acc5d8c40dd9a75840c57c1d3e65cac29
Martijn van Dijk <mhvdijk@gmail.com>
2020-09-08 16:32:44 +0200

    Add shared assembly method

---
+++ Lottie.Forms/AnimationViewExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottie.Forms
+{
+    public static class AnimationViewExtensions
+    {
+        public static Stream GetStreamFromAssembly(this AnimationView animationView)
+        {
+            if (animationView.Animation is string embeddedAnimation)
+            {
+                var assembly = Xamarin.Forms.Application.Current.GetType().Assembly;
+                var stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{embeddedAnimation}");
+
+                if (stream == null)
+                {
+                    return null;
+                    //throw new FileNotFoundException("Cannot find file.", embeddedAnimation);
+                }
+                return stream;
+            }
+            return null;
+        }
+    }
+}
